bcfb9c971250d10e2d4ff0906c8f6cb9382f5fa2
sharon Dahan <sharon.d@spotinst.com>
2018-07-31 16:57:42 +0300

    [src] fix build

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 
 OS="darwin linux windows"
 ARCH="amd64"
+VERSION="0.2"
 
 echo "Getting build dependencies"
 go get -t github.com/docker-machine-driver-spotinst
@@ -11,7 +12,7 @@ go get -t github.com/docker-machine-driver-spotinst
 for GOOS in $OS; do
     for GOARCH in $ARCH; do
         arch="$GOOS-$GOARCH"
-        binary="bin/docker-machine-driver-spotinst.$arch"
+        binary="bin/docker-machine-driver-spotinst."$VERSION".$arch"
         echo "Building $binary"
         GOOS=$GOOS GOARCH=$GOARCH go build -gcflags=-trimpath=$GOPATH -asmflags=-trimpath=$GOPATH -o $binary github.com/docker-machine-driver-spotinst
     done
@@ -19,4 +20,4 @@ done
 
 echo "Install adapter in local conputer"
 
-cp bin/docker-machine-driver-spotinst.darwin-amd64 /usr/local/bin/docker-machine-driver-spotinst
+cp "bin/docker-machine-driver-spotinst."$VERSION".darwin-"$ARCH "/usr/local/bin/docker-machine-driver-spotinst"
